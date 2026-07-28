6bf8afceff820714b969b539540adf5353631f2b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-28 14:02:20 +0300

    add Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+import { useEffect, type ReactNode } from "react";
+
+import css from "./Modal.module.css";
+import { createPortal } from "react-dom";
+
+interface ModalProps {
+  children: ReactNode;
+  onClose: () => void;
+}
+
+export default function Modal({ children, onClose }: ModalProps) {
+  function handleBackdropClick(event: React.MouseEvent<HTMLDivElement>) {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  }
+
+  useEffect(() => {
+    const handleEscapeKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+    document.addEventListener("keydown", handleEscapeKeyDown);
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKeyDown);
+      document.body.style.overflow = "";
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div
+      className={css.backdrop}
+      role="dialog"
+      aria-modal="true"
+      onClick={handleBackdropClick}
+    >
+      <div className={css.modal}>{children}</div>
+    </div>,
+    document.body,
+  );
+}
