400365cf59670ca4f83fbbe09d4ac823c2d22a0f
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-28 21:29:09 +0300

    add modern-normalize

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 
+import "modern-normalize"
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
