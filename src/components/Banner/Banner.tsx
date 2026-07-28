955a9906019a69eeae6fec12aab1a7820ad373d9
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-28 21:42:31 +0300

    add Banner

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,20 @@
+import clsx from "clsx";
+
+import css from "./Banner.module.css";
+
+interface BannerProps {
+  text: string;
+  type?: "log" | "info" | "warning" | "error";
+}
+
+export default function Banner({ text, type = "log" }: BannerProps) {
+  const classes = clsx(
+    css.text,
+    type === "log" && css.log,
+    type === "info" && css.info,
+    type === "warning" && css.warning,
+    type === "error" && css.error,
+  );
+
+  return <p className={classes}>{text}</p>;
+}
