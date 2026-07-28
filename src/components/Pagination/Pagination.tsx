77b58620b8499414cd5faa0f7294e79711a8bd85
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-07-28 14:02:34 +0300

    add Pagination

---
+++ src/components/Pagination/Pagination.tsx
@@ -0,0 +1,39 @@
+import css from "./Pagination.module.css";
+
+// react-paginate setup
+import type { ReactPaginateProps } from "react-paginate";
+import type { ComponentType } from "react";
+import ReactPaginateModule from "react-paginate";
+type ModuleWithDefault<T> = { default: T };
+const ReactPaginate = (
+  ReactPaginateModule as unknown as ModuleWithDefault<
+    ComponentType<ReactPaginateProps>
+  >
+).default;
+
+interface PaginationProps {
+  totalPages: number;
+  currentPage: number;
+  onPageChange: (nextPage: number) => void;
+}
+
+export default function Pagination({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected: selectedPaginationIndex }) => {
+        onPageChange(selectedPaginationIndex + 1);
+      }}
+      forcePage={currentPage - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+}
