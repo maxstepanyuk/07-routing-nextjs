9772ad5c7ff23c13aca430276665353ea396925b
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-06 08:39:24 +0300

    add components Banner Modal NoteForm NoteList Pagination SearchBox

---
+++ 06-notehub-nextjs/components/NoteForm/NoteForm.tsx
@@ -0,0 +1,142 @@
+import { ErrorMessage, Field, Form, Formik, type FormikHelpers } from "formik";
+import * as Yup from "yup";
+
+import css from "./NoteForm.module.css";
+import type { CreateNote, NoteTag } from "../../types/note";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { createNote } from "@/lib/api";
+
+const initialValues: CreateNote = {
+  title: "",
+  content: "",
+  tag: "Todo",
+};
+
+const validTags: NoteTag[] = [
+  "Todo",
+  "Work",
+  "Personal",
+  "Meeting",
+  "Shopping",
+];
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string().min(3).max(50).required(),
+  content: Yup.string().max(500),
+  tag: Yup.string().oneOf(validTags).required(),
+});
+
+interface NoteFormProps {
+  onCancel: () => void;
+}
+
+export default function NoteForm({ onCancel }: NoteFormProps) {
+  const queryClient = useQueryClient();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: createNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["notes"],
+      });
+      onCancel();
+    },
+    onError: (e) => {
+      const msg = "Error creating note";
+      console.log(msg, e);
+      toast.error(msg);
+    },
+  });
+
+  const handleSubmit = (
+    formValues: CreateNote,
+    actions: FormikHelpers<CreateNote>,
+  ) => {
+    mutate(formValues, {
+      onSuccess: () => {
+        actions.resetForm();
+      },
+    });
+  };
+
+  return (
+    <>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        {({ isValid, dirty }) => {
+          return (
+            <Form className={css.form}>
+              <div className={css.formGroup}>
+                <label htmlFor="title">Title</label>
+                <Field
+                  id="title"
+                  type="text"
+                  name="title"
+                  className={css.input}
+                />
+                <ErrorMessage
+                  component="span"
+                  name="title"
+                  className={css.error}
+                />
+              </div>
+
+              <div className={css.formGroup}>
+                <label htmlFor="content">Content</label>
+                <Field
+                  as="textarea"
+                  id="content"
+                  name="content"
+                  rows={8}
+                  className={css.textarea}
+                />
+                <ErrorMessage
+                  component="span"
+                  name="content"
+                  className={css.error}
+                />
+              </div>
+
+              <div className={css.formGroup}>
+                <label htmlFor="tag">Tag</label>
+                <Field as="select" id="tag" name="tag" className={css.select}>
+                  <option value="Todo">Todo</option>
+                  <option value="Work">Work</option>
+                  <option value="Personal">Personal</option>
+                  <option value="Meeting">Meeting</option>
+                  <option value="Shopping">Shopping</option>
+                </Field>
+                <ErrorMessage
+                  component="span"
+                  name="tag"
+                  className={css.error}
+                />
+              </div>
+
+              <div className={css.actions}>
+                <button
+                  type="button"
+                  className={css.cancelButton}
+                  onClick={onCancel}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="submit"
+                  className={css.submitButton}
+                  disabled={!isValid || !dirty || isPending}
+                >
+                  {isPending ? "Creating..." : "Create note"}
+                </button>
+              </div>
+            </Form>
+          );
+        }}
+      </Formik>
+    </>
+  );
+}
---
+++ 06-notehub-nextjs/components/Pagination/Pagination.tsx
@@ -0,0 +1,25 @@
+import ReactPaginate from "react-paginate";
+
+import css from "./Pagination.module.css";
+
+export default function Pagination({
+  totalPages,
+  currentPage,
+  onPageChange,
+}: PaginationProps) {
+  return (
+    <ReactPaginate
+      pageCount={totalPages}
+      pageRangeDisplayed={5}
+      marginPagesDisplayed={1}
+      onPageChange={({ selected: selectedPaginationIndex }) => {
+        onPageChange(selectedPaginationIndex + 1);
+      }}
+      forcePage={currentPage - 1}
+      containerClassName={css.pagination}
+      activeClassName={css.active}
+      nextLabel="→"
+      previousLabel="←"
+    />
+  );
+}
