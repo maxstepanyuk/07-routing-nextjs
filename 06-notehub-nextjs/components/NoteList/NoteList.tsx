991ef761b189576c9aeac0579aef4aa2106250e1
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-06 12:23:02 +0300

    upd NoteList add Link

--- 06-notehub-nextjs/components/NoteList/NoteList.tsx
+++ 06-notehub-nextjs/components/NoteList/NoteList.tsx
@@ -3,6 +3,7 @@ import type { Note } from "../../types/note";
 import css from "./NoteList.module.css";
 import toast from "react-hot-toast";
 import { deleteNote } from "@/lib/api";
+import Link from "next/link";
 
 interface NoteListProps {
   notes: Note[];
@@ -38,6 +39,7 @@ export default function NoteList({ notes }: NoteListProps) {
             <p className={css.content}>{note.content}</p>
             <div className={css.footer}>
               <span className={css.tag}>{note.tag}</span>
+              <Link href={"/notes/" + note.id} className={css.link}>View details</Link>
               <button
                 className={css.button}
                 onClick={() => handleDelete(note.id)}
