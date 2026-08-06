e60e9b8658b6b13b666e30cc6eaf6568da0d0df1
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-06 09:22:35 +0300

    upd Banner add positionStatic=false prop

--- 06-notehub-nextjs/components/Banner/Banner.tsx
+++ 06-notehub-nextjs/components/Banner/Banner.tsx
@@ -5,20 +5,31 @@ import css from "./Banner.module.css";
 interface BannerProps {
   text: string;
   type?: "log" | "info" | "warning" | "error";
+  positionStatic?: boolean;
 }
 
-export default function Banner({ text, type = "log" }: BannerProps) {
-  const classes = clsx(
+export default function Banner({
+  text,
+  type = "log",
+  positionStatic = false,
+}: BannerProps) {
+  const pClasses = clsx(
     css.text,
+    !positionStatic && css.textAbsolute,
     type === "log" && css.log,
     type === "info" && css.info,
     type === "warning" && css.warning,
     type === "error" && css.error,
   );
 
+  const divClasses = clsx(
+    css.container,
+    !positionStatic && css.containerRelative,
+  );
+
   return (
-    <div className={css.container}>
-      <p className={classes}>{text}</p>
+    <div className={divClasses}>
+      <p className={pClasses}>{text}</p>
     </div>
   );
 }
