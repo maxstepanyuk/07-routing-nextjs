65c3387122738da053c2d411f37b4ef01b8d54fb
maxstepanyuk - main PC <40894200+maxstepanyuk@users.noreply.github.com>
2026-08-10 08:33:07 +0300

    add @interceptionModal slot

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,16 +1,19 @@
 type NotesLayoutProps = {
   children: React.ReactNode;
   sidebar: React.ReactNode;
+  interceptionModal: React.ReactNode;
 };
 
 export default function NotesFilterLayout({
   children,
   sidebar,
+  interceptionModal,
 }: Readonly<NotesLayoutProps>) {
   return (
     <>
       {sidebar}
       {children}
+      {interceptionModal}
     </>
   );
 }
